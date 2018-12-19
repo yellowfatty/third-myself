2e203e068cca9da8a3d172f8168305d5ae4affe4
yellowfatty <huapeng1997@qq.com>
2018-12-19 09:57:37 +0800

    添加订单实体类和service接口

---
+++ src/main/java/com/qf/admin/pojo/po/Order.java
@@ -0,0 +1,64 @@
+package com.qf.admin.pojo.po;
+
+public class Order {
+//    订单实体类
+    private int orderId;
+    private String orderNo;
+    private String tenementname;
+    private String tenementserial;
+    private String phone;
+
+    @Override
+    public String toString() {
+        return "Orderservice{" +
+                "orderId=" + orderId +
+                ", orderNo='" + orderNo + '\'' +
+                ", tenementname='" + tenementname + '\'' +
+                ", tenementserial='" + tenementserial + '\'' +
+                ", phone='" + phone + '\'' +
+                '}';
+    }
+
+    public Order() {
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getOrderNo() {
+        return orderNo;
+    }
+
+    public void setOrderNo(String orderNo) {
+        this.orderNo = orderNo;
+    }
+
+    public String getTenementname() {
+        return tenementname;
+    }
+
+    public void setTenementname(String tenementname) {
+        this.tenementname = tenementname;
+    }
+
+    public String getTenementserial() {
+        return tenementserial;
+    }
+
+    public void setTenementserial(String tenementserial) {
+        this.tenementserial = tenementserial;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
---
+++ src/main/java/com/qf/admin/service/Orderservice.java
@@ -0,0 +1,16 @@
+package com.qf.admin.service;
+
+import com.alibaba.fastjson.JSONObject;
+import com.qf.admin.pojo.po.Order;
+
+public interface Orderservice {
+    //订单service
+    JSONObject listtoroder(JSONObject jsonObject);
+
+
+    Object singleroder(int orderId);
+
+    void updateorder(Order order);
+
+    int deleteroder(int orderId);
+}
